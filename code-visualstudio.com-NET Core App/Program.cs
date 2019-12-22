c6184956adca724ff8be6a00c472c34c17472521
Nico Leputois <nicoleputois@outlook.com>
2019-12-22 09:39:53 +0100

    Ajoutez des fichiers projet.

---
+++ code-visualstudio.com-NET Core App/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime;
+
+namespace code_visualstudio.com_NET_Core_App
+{
+    class Program
+    {
+        private const string Value = "String Value";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Value);
+            Console.ReadLine();
+                }
+    }
+}
